74ba481c69f3a1990be9ab74212c9f65c59ed04c
RK <113088626+razveen63@users.noreply.github.com>
2023-06-25 17:34:18 +0400

    Multithreaded Implementation of equalization
    Running the equalization process via multithreading process

--- src/partA/MultiThreading.java
+++ src/partA/MultiThreading.java
@@ -6,119 +6,203 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.image.DataBufferByte;
 
-//main class
 public class MultiThreading {
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		String originalImagePath = "C:\\\\\\\\Users\\\\\\\\razve\\\\\\\\OneDrive\\\\\\\\Desktop\\\\\\\\331_project\\\\\\\\Rain_Tree1.jpg";
-		String greyImagePath = "C:\\\\\\\\Users\\\\\\\\razve\\\\\\\\OneDrive\\\\\\\\Desktop\\\\\\\\331_project\\\\\\\\GRAYED_Rain_Tree1.jpg";
-		
-		BufferedImage greyImg = gImage(originalImagePath, greyImagePath);
-		int[][] array = convertTo2DArray(greyImg);
-		int[] oneArray = convertTo1DArray(array);
-		
-		
-	}//main ends here
-	
-	// get gray scale image 
-	public static BufferedImage gImage(String originalFile, String grayFile) {
-		 BufferedImage img = null;
-		    File f = null;
-		    File f1 = null;
+	    String originalImagePath = "C:\\Users\\razve\\OneDrive\\Desktop\\331_project\\rk_331_Q1\\Rain_Tree1.jpg";
+	    String greyImagePath = "C:\\Users\\razve\\OneDrive\\Desktop\\331_project\\rk_331_Q1\\GRAYED_Rain_Tree1.jpg";
+	    String equalImagePath = "C:\\Users\\razve\\OneDrive\\Desktop\\331_project\\rk_331_Q1\\EQ_Rain_Tree1.jpg";
 
-		    //read image
-		    try{
-		      f = new File(originalFile);
-		      img = ImageIO.read(f);
-		    }
-		    catch(IOException e){
-		      System.out.println("File not Found!");
-		    }
+	    BufferedImage greyImg = gImage(originalImagePath, greyImagePath);
+	    byte[][] array = convertTo2DArray(greyImg);
+	    byte[] oneArray = convertTo1DArray(array);
 
-		    //get image width and height
-		    int width = img.getWidth();
-		    int height = img.getHeight();
+	    int[] histogram = new int[256];
 
-		    //convert to gray scale
-		    for(int y = 0; y < height; y++){
-		      for(int x = 0; x < width; x++){
-		        int p = img.getRGB(x,y);
+	    final int numOfThreads = 50;
+	    int totalPix = greyImg.getHeight() * greyImg.getWidth();
+	    final int pixThread = totalPix / numOfThreads;
+	    int start = 0;
+	    int end = 0;
 
-		        int a = (p>>24)&0xff;
-		        int r = (p>>16)&0xff;
-		        int g = (p>>8)&0xff;
-		        int b = p&0xff;
+	    Thread[] threads = new Thread[numOfThreads];
 
-		        //calculate average
-		        int avg = (r+g+b)/3;
+	    for (int r = 0; r < numOfThreads; r++) {
+	        start = r * pixThread;
+	        end = (r == numOfThreads - 1) ? totalPix - 1 : ((r + 1) * pixThread) - 1;
+	        threads[r] = new Thread(new EqualizeRunnable(start, end, oneArray, histogram));
+	        threads[r].start();
+	    }
 
-		        //replace RGB value with average
-		        p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+	    try {
+	        for (Thread t : threads) {
+	            t.join();
+	        }
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
 
-		        img.setRGB(x, y, p);
-		      }
-		    }
+	    int[] cumulativeHist = new int[256];
+	    cumulativeHist[0] = histogram[0];
+	    for (int i = 1; i < 256; i++) {
+	        cumulativeHist[i] = cumulativeHist[i - 1] + histogram[i];
+	    }
 
-		    //write image
-		    try{
-		      f1 = new File(grayFile);
-		      ImageIO.write(img, "jpg", f1);
-		    }catch(IOException e){
-		      System.out.println("File couldn't be written!");
-		    }
-		    
-		    //pass the buffered image to convert it to array
-		    return img;
-	}//Buffered Image ends here
-	
-	
-	// convert the buffered image into an array
-	public static int[][] convertTo2DArray(BufferedImage img) {
-        
-    final byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData(); // get pixel value as single array from buffered Image
-    final int width = img.getWidth(); //get image width value
-    final int height = img.getHeight(); //get image height value
-    int[][] result = new int[height][width]; //Initialize the array with height and width
-    
-    
-    
-    //this loop allocates pixels value to two dimensional array
-    for (int pixel = 0, row = 0, col = 0; row < height; row++) {
-        for (int col1 = 0; col1 < width; col1++, pixel++) {
-            int argb = pixels[pixel] & 0xFF;
-            result[row][col1] = argb;
+	    byte[] equalizedValues = new byte[totalPix];
+	    for (int i = 0; i < totalPix; i++) {
+	        int pixelValue = oneArray[i] & 0xFF;
+	        equalizedValues[i] = (byte) ((cumulativeHist[pixelValue] * 255) / totalPix);
+	    }
+
+	    BufferedImage equalizedImage = convertToBufferedImage(equalizedValues, greyImg.getWidth(), greyImg.getHeight());
+
+	    try {
+	        File equalizedFile = new File(equalImagePath);
+	        ImageIO.write(equalizedImage, "jpg", equalizedFile);
+	    } catch (IOException e) {
+	        System.out.println("Equalized file couldn't be written!");
+	    }
+	}
+
+
+    public static BufferedImage gImage(String originalFile, String grayFile) {
+        BufferedImage img = null;
+        File f = null;
+        File f1 = null;
+
+        try {
+            f = new File(originalFile);
+            img = ImageIO.read(f);
+        } catch (IOException e) {
+            System.out.println("File not Found!");
+        }
+
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        BufferedImage grayImg = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        grayImg.getGraphics().drawImage(img, 0, 0, null);
+
+        try {
+            f1 = new File(grayFile);
+            ImageIO.write(grayImg, "jpg", f1);
+        } catch (IOException e) {
+            System.out.println("File couldn't be written!");
+        }
+
+        return grayImg;
+    }
+
+    public static byte[][] convertTo2DArray(BufferedImage img) {
+        final byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+        final int width = img.getWidth();
+        final int height = img.getHeight();
+        byte[][] result = new byte[height][width];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                int pixel = (row * width + col);
+                result[row][col] = pixels[pixel];
+            }
+        }
+
+        return result;
+    }
+
+    public static byte[] convertTo1DArray(byte[][] array2D) {
+        int totalElements = array2D.length * array2D[0].length;
+        byte[] result = new byte[totalElements];
+        int c = 0;
+
+        for (int k = 0; k < array2D.length; k++) {
+            for (int m = 0; m < array2D[k].length; m++) {
+                result[c] = array2D[k][m];
+                c++;
+            }
+        }
+
+        return result;
+    }
+
+    public static byte[] allHistEQ(int start, int end, byte[] oneArray) {
+        int L = 255;
+        int size = (end - start + 1);
+
+        int[] histogram = new int[L + 1];
+        for (int i = start; i <= end; i++) {
+            int pixelValue = oneArray[i] & 0xFF;
+            histogram[pixelValue]++;
+        }
+
+        int[] cumulativeHist = new int[L + 1];
+        cumulativeHist[0] = histogram[0];
+        for (int i = 1; i <= L; i++) {
+            cumulativeHist[i] = cumulativeHist[i - 1] + histogram[i];
+        }
+
+        byte[] equalizedValues = new byte[size];
+        for (int i = start; i <= end; i++) {
+            int pixelValue = oneArray[i] & 0xFF;
+            equalizedValues[i - start] = (byte) ((cumulativeHist[pixelValue] * L) / (end - start + 1));
+        }
+
+        return equalizedValues;
+    }
+
+
+    public static byte[] concatenateArrays(byte[][] arrays) {
+        int totalLength = 0;
+        for (byte[] array : arrays) {
+            totalLength += array.length;
+        }
+
+        byte[] result = new byte[totalLength];
+        int currentIndex = 0;
+
+        for (byte[] array : arrays) {
+            System.arraycopy(array, 0, result, currentIndex, array.length);
+            currentIndex += array.length;
+        }
+
+        return result;
+    }
+
+    private static class EqualizeRunnable implements Runnable {
+        private final int start;
+        private final int end;
+        private final byte[] oneArray;
+        private final int[] histogram;
+
+        public EqualizeRunnable(int start, int end, byte[] oneArray, int[] histogram) {
+            this.start = start;
+            this.end = end;
+            this.oneArray = oneArray;
+            this.histogram = histogram;
+        }
+
+        @Override
+        public void run() {
+            for (int i = start; i <= end; i++) {
+                int pixelValue = oneArray[i] & 0xFF;
+                synchronized (histogram) {
+                    histogram[pixelValue]++;
+                }
+            }
         }
     }
- // Print the values of the result array
-   /* for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            System.out.print(result[i][j] + " ");
+
+    public static BufferedImage convertToBufferedImage(byte[] outArray, int width, int height) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        byte[] imagePixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+
+        for (int i = 0; i < outArray.length; i++) {
+            int row = i / width;
+            int col = i % width;
+            int pixelValue = outArray[i] & 0xFF;
+            imagePixels[row * width + col] = (byte) pixelValue;
         }
-        System.out.println();
-    }*/
-    return result; //return the result as two dimensional array
-	} //convert to 2D array ends here
-	
-	
-	// define method which converts the 2D array into 1D array
-	public static int[] convertTo1DArray(int[][]array2D) {
 
-		int totalElements = array2D.length * array2D[0].length;
-	    int[] b = new int[totalElements];
-	    int c = 0;
+        return image;
+    }
 
-	    for (int k = 0; k < array2D.length; k++) {
-	        for (int m = 0; m < array2D[k].length; m++) {
-	            b[c] = array2D[k][m];
-	            System.out.println(b[c]);
-	            c++;
-	        }
-	    }
-	    return b;
-	}//convert to 1D Array ends here
-	
-	
-	
-   }//class ends here
+}
